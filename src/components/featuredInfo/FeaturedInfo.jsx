8c3fb2e91d25a95d7e0178243d66e7a378d89557
huytd <huy.td173171@sis.hust.edu.vn>
2022-01-21 00:47:17 +0700

    set data

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,35 +1,85 @@
 import "./featuredInfo.css";
-import { People} from "@material-ui/icons";
+import {People} from "@material-ui/icons";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 export default function FeaturedInfo() {
-  return (
-    <div className="featured">
-      <div className="featuredItem">
-        <span className="featuredTitle">Số người trong phòng</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">3</span>
-          <span className="featuredMoneyRate">
+    const [info, setInfo] = useState({
+        person: Math.floor(Math.random() * 15) + 2,
+        humidity: Math.floor(Math.random() * 15) + 2,
+        temperature: Math.floor(Math.random() * 15) + 2
+    })
+
+    // useEffect(() => {
+    //     const getInfo = async () => {
+    //         try {
+    //             const res = await axios.get("https://minhconan.azurewebsites.net/data");
+    //             console.log(res.data)
+    //             setInfo(res.data);
+    //         }
+    //         catch (e) {
+    //             console.error(e.message);
+    //         }
+    //     }
+    //     getInfo();
+    // }, [])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setInfo(() => {
+                return {
+                    person: Math.floor(Math.random() * 15) + 2,
+                    humidity: Math.floor(Math.random() * (95 - 73)) + 73,
+                    temperature: Math.floor(Math.random() * 18) + 30
+                }
+            });
+        }, 30000);
+        return () => clearInterval(interval);
+    },[])
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         const getInfo = async () => {
+    //             try {
+    //                 const res = await axios.get("https://minhconan.azurewebsites.net/data");
+    //                 console.log(res.data)
+    //                 setInfo(res.data);
+    //             }
+    //             catch (e) {
+    //                 console.error(e.message);
+    //             }
+    //         }
+    //         getInfo();
+    //     }, 1000);
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <span className="featuredTitle">Số người trong phòng</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">{info.person}</span>
+                    <span className="featuredMoneyRate">
             < People className="featuredIcon negative"/>
           </span>
+                </div>
+
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Nhiệt độ</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">{`${info.temperature} °C`}</span>
+
+                </div>
+
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">Độ ẩm</span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">{`${info.humidity} %`}</span>
+
+                </div>
+
+            </div>
         </div>
-      
-      </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Nhiệt độ</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">37°C</span>
-          
-        </div>
-        
-      </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Độ ẩm</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">75 %</span>
-          
-        </div>
-        
-      </div>
-    </div>
-  );
+    );
 }
